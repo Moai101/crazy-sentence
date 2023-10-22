20715d14ce938588bdb7626cf4eb7b54797adf18
beero-hattori <hattori@beero.co.jp>
2023-10-22 20:25:55 +0900

    fix:　文章取得処理の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,99 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+
+const  REACT_APP_SCRIPT = process.env.REACT_APP_SCRIPT
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-                {process.env.REACT_APP_HELLO_WORLD}
 
-        </a>
+    const [images,setImages] = useState<any[]>([])
+
+  useEffect(()=>{
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+
+
+
+
+    if(REACT_APP_SCRIPT){
+
+      // axios.get(randamUrl)
+
+
+      axios({
+        method: 'GET',
+        url:  REACT_APP_SCRIPT
+    })
+    .then(response => {
+        
+
+        setImages([...response.data.message])
+    })
+    .catch(error => {
+        console.error('Error: ', error.response ? error.response.data : error.message);
+    });
+    
+
+
+    }
+
+
+
+    
+  },[REACT_APP_SCRIPT])
+  return (
+    <div >
+      <header className="App-header">
+
+        <div   style={{
+    maxWidth:700,
+    display:"flex"
+  }}>
+
+            
+{images.map((value,i)=>{
+
+return (
+
+  <div key={i}>
+  <div
+
+
+  
+  >
+  <img src={value.imageUrl}  alt="logo" />
+
+
+  </div>
+  <div>
+
+    {value.sentence}
+
+  </div>
+  
+
+</div>
+
+)
+
+
+
+
+})}
+
+
+  </div>
+
+
+  
       </header>
+
+
+
+
     </div>
   );
 }
