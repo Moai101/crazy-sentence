cddb323b6819c27a13eeb409a5794b63234b756e
beero-hattori <hattori@beero.co.jp>
2023-10-24 20:06:47 +0900

    文章添削アプリの完成

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ import axios from 'axios';
 
 
 const  REACT_APP_SCRIPT = process.env.REACT_APP_SCRIPT
+const REACT_APP_POST_SCRIPT = process.env.REACT_APP_POST_SCRIPT
 
 
 function App() {
 
-    const [images,setImages] = useState<any[]>([])
+
+  const [japaneseArray,setJapaneseArray] = useState<string[]>([])
+  const [englishSentence,setEnglishSentence] = useState<string[]>([])
+  const [num,setNum] = useState<number>(0)
+  const [row,setRow] = useState<number>(0)
+
 
   useEffect(()=>{
 
@@ -23,18 +29,31 @@ function App() {
       // axios.get(randamUrl)
 
 
-      axios({
-        method: 'GET',
-        url:  REACT_APP_SCRIPT
-    })
-    .then(response => {
-        
 
-        setImages([...response.data.message])
-    })
-    .catch(error => {
-        console.error('Error: ', error.response ? error.response.data : error.message);
-    });
+
+        axios.get("https://script.google.com/macros/s/AKfycbwKfkIkrUCtuHm_CuRFrNJYPqPiA3-zXfNTlbDY8m8YyKtd9jTYtPILeSPoRQSm5LWUWw/exec")
+        .then(response => {
+    
+          const data = response.data
+          
+          const japaneseSentence = data.japaneseSentence.split("\n")
+          const englishSentence = data.englishSentence.split("\n")
+          const rowNumber = data.rowNumber
+
+          setJapaneseArray([...japaneseSentence])
+          setEnglishSentence([...englishSentence])
+          setRow(rowNumber)
+
+
+    
+        })
+        .catch(error => {
+            console.error('Error: ', error.response ? error.response.data : error.message);
+        });
+
+
+
+
     
 
 
@@ -44,48 +63,88 @@ function App() {
 
     
   },[REACT_APP_SCRIPT])
+
+  const complete = async()=>{
+
+    let status = "OK"
+
+    if(num === -1){
+      status = "NG"
+    }
+
+    const data = {
+      status:status,
+      sentence:englishSentence[num],
+      rowNumber:row
+
+      
+    }
+
+    if(REACT_APP_POST_SCRIPT){
+
+      const result = await axios.post(REACT_APP_POST_SCRIPT,JSON.stringify(data))
+
+      if(result.data.status === "OK"){
+  
+        alert("更新が完了しました")
+  
+      }
+
+    }
+
+
+
+
+
+  }
   return (
     <div >
-      <header className="App-header">
+      <header className="App-header"
+      
+      >
 
-        <div   style={{
-    maxWidth:700,
-    display:"flex"
-  }}>
 
-            
-{images.map((value,i)=>{
+<ul>
+
+{japaneseArray.map((value,i)=>{
 
 return (
-
-  <div key={i}>
-  <div
-
-
-  
-  >
-  {/* <img src={value.imageUrl}  alt="logo" /> */}
+  <li key={i}>
+    <input type={"checkbox"} checked={i === num} value={i} onChange={(e)=>{
+      const value = Number(e.target.value)
+      setNum(value)
 
 
-  </div>
-  <div>
-
-    {value.sentence}
-
-  </div>
-  
-
-</div>
+    }}/>{value}
+  </li>
 
 )
-
-
-
-
 })}
 
+<li>
 
-  </div>
+<li>
+    <input type={"checkbox"} value={-1} onChange={(e)=>{
+      const value = Number(e.target.value)
+      setNum(value)
+
+
+    }}/>{"NG"}
+  </li>
+
+  <button onClick={complete}>完了する</button>
+
+
+</li>
+
+
+</ul>
+
+
+            
+
+
+
 
 
   
